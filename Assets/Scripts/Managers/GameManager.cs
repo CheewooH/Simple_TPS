86ce98d57454854e18f61fd12e0c91912c838d27
CHEEWOO HONG <1974cldn@gmail.com>
2025-05-15 12:59:05 +0900

    [Feat] 게임매니저, 오디오매니저

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,8 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using DesignPattern;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
+    private void Awake() => Init();
     
+    private void Init()
+    {
+        base.SingletonInit();
+    }
 }
