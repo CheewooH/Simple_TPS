5a9ff7ceaca74c5fb6f957c6833e949d603b5da9
CHEEWOO HONG <1974cldn@gmail.com>
2025-05-15 15:24:57 +0900

    [Feat] AudioManager

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -3,21 +3,38 @@ using System.Collections.Generic;
 using DesignPattern;
 using UnityEngine;
 
+
 public class AudioManager : MonoBehaviour
 {
     private AudioSource _bgmSource;
+    private ObjectPool _sfxPool;
 
-    private ObjectPool<PlayerStatus> _pool = new();
+    [SerializeField] private List<AudioClip> _bgmList = new();
+    [SerializeField] private SFXController _sfxPrefab;
+
 
     private void Awake() => Init();
 
     private void Init()
     {
         _bgmSource = GetComponent<AudioSource>();
+
+        _sfxPool = new ObjectPool(transform, _sfxPrefab, 10);
     }
 
-    public void BgmPlay()
+    public void BgmPlay(int index)
     {
-        _bgmSource.Play();
+        if (0 <= index && index < _bgmList.Count)
+        {
+            _bgmSource.Stop();
+            _bgmSource.clip = _bgmList[index];
+            _bgmSource.Play();
+        }
+    }
+    public SFXController GetSFX()
+    {
+        // 풀에서 꺼내와서 반환
+        PooledObject po = _sfxPool.PopPool();
+        return po as SFXController;
     }
 }
